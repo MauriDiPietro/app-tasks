f21e360d51c7efbf063e7510626508c799472b4f
MauriDiPietro <dipietro.jm@gmail.com>
2021-07-11 23:23:23 -0300

    database-ok

--- src/database.js
+++ src/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect()
+mongoose.connect('mongodb://localhost/mern-tasks')
+    .then(db =>console.log('DB is connected'))
+    .catch(err=>console.error(err));
+
+    module.exports = mongoose;
